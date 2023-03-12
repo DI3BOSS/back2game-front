804099da4080ea4100e791e3108da4dbaff1a326
DI3BOSS <diegofbermudez@gmail.com>
2023-03-12 03:49:44 +0100

    Implement the rendering of Loader and LogInForm to check  validators

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import FeedbackModal from "./components/FeedbackModal/FeedbackModal";
 import Loader from "./components/Loader/Loader";
+import LogInForm from "./components/LogInForm/LogInForm";
+import { useAppSelector } from "./store/hooks";
+
+import { ReactComponent as Icon } from "./assets/icons/xButton.svg";
 
 const App = (): JSX.Element => {
+  const {
+    isLoading,
+    feedbackModal: { isWrong, message, title },
+  } = useAppSelector((state) => state.ui);
+
   return (
     <>
-      <Loader />
+      {isLoading && <Loader />}
+      {isWrong && (
+        <FeedbackModal title={title} message={message} icon={<Icon />} />
+      )}
+      <LogInForm />
     </>
   );
 };
