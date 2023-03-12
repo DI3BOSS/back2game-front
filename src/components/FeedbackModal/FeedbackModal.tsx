e99c40055137bc42c073e551b6f308c4d6a3c6b5
DI3BOSS <diegofbermudez@gmail.com>
2023-03-12 02:30:40 +0100

    Add funcionality to FeedbackModal component

--- src/components/FeedbackModal/FeedbackModal.tsx
+++ src/components/FeedbackModal/FeedbackModal.tsx
@@ -1,3 +1,5 @@
+import { showFeedbackActionCreator } from "../../store/features/uiSlice/uiSlice";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import Button from "../Button/Button";
 import FeedbackModalStyled from "./FeedbackModalStyled";
 
@@ -12,16 +14,30 @@ const FeedbackModal = ({
   message,
   icon,
 }: FeedBackModalProps): JSX.Element => {
+  const {
+    feedbackModal: { isWrong },
+  } = useAppSelector((state) => state.ui);
+
+  const dispatch = useAppDispatch();
+
+  const handleClose = () => {
+    dispatch(showFeedbackActionCreator({ isWrong: false }));
+  };
+
   return (
-    <FeedbackModalStyled>
-      <span className="modal-feedback__message">
-        <div className="modal-feedback__head">
-          <span className="modal-feedback__title">{title}</span>
-          <Button icon={icon} className={"modal-feedback__close-button"} />
-        </div>
-        <span>{message}</span>
-      </span>
-    </FeedbackModalStyled>
+    <>
+      {isWrong && (
+        <FeedbackModalStyled onClick={handleClose}>
+          <span className="modal-feedback__message">
+            <div className="modal-feedback__head">
+              <span className="modal-feedback__title">{title}</span>
+              <Button icon={icon} className={"modal-feedback__close-button"} />
+            </div>
+            <span>{message}</span>
+          </span>
+        </FeedbackModalStyled>
+      )}
+    </>
   );
 };
 
