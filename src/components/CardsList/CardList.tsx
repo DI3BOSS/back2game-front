d8e42417ee5560df91c2cb48bbaa94175ec7057a
DI3BOSS <diegofbermudez@gmail.com>
2023-03-16 00:33:13 +0100

    test the CardList listitem key corner case

--- src/components/CardsList/CardList.tsx
+++ src/components/CardsList/CardList.tsx
@@ -1,25 +1,20 @@
-import { useAppSelector } from "../../store/hooks";
 import Card from "../Card/Card";
 import { ReactComponent as ViewIcon } from "../../assets/icons/view.svg";
 import { ReactComponent as EditIcon } from "../../assets/icons/edit.svg";
 import { ReactComponent as DeleteIcon } from "../../assets/icons/delete.svg";
-import useGames from "../../hooks/useGames/useGames";
-import { useEffect } from "react";
 import CardListStyled from "./CardListStyled";
+import { GamesStructure } from "../../store/features/gamesSlice/types";
 
-const CardList = (): JSX.Element => {
-  const { getGames } = useGames();
-  useEffect(() => {
-    getGames();
-  }, [getGames]);
+interface CardListProps {
+  games: GamesStructure;
+}
 
-  const gameCards = useAppSelector((state) => state.games);
-
+const CardList = ({ games }: CardListProps): JSX.Element => {
   return (
     <>
       <CardListStyled>
-        {gameCards.map((game) => (
-          <li key={game.cover}>
+        {games.map((game) => (
+          <li key={game.cover} className="card">
             <Card
               game={game}
               deleteButton={<DeleteIcon />}
@@ -28,7 +23,7 @@ const CardList = (): JSX.Element => {
             />
           </li>
         ))}
-        <h2 className="shown-games">Showing {gameCards.length} games</h2>
+        <h2 className="shown-games">Showing {games.length} games</h2>
       </CardListStyled>
     </>
   );
