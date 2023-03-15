bca0c5cb2254e4cbec0a6e9331b0ea10633dcfb3
DI3BOSS <diegofbermudez@gmail.com>
2023-03-16 00:36:29 +0100

    Move CardList functionality to HomePage in order to fix it testing

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import CardList from "../../components/CardsList/CardList";
+import useGames from "../../hooks/useGames/useGames";
+import { useAppSelector } from "../../store/hooks";
 
 const HomePage = (): JSX.Element => {
-  return <CardList />;
+  const { getGames } = useGames();
+  useEffect(() => {
+    getGames();
+  }, [getGames]);
+
+  const gameCards = useAppSelector((state) => state.games);
+
+  return <CardList games={gameCards} />;
 };
 
 export default HomePage;
