9f1a4f809535757664e118b4aac7d9833ff8efb3
DI3BOSS <diegofbermudez@gmail.com>
2023-03-22 12:38:11 +0100

    Refactor feedbackModal styles to support success feeback

--- src/components/FeedbackModal/FeedbackModalStyled.tsx
+++ src/components/FeedbackModal/FeedbackModalStyled.tsx
@@ -14,6 +14,7 @@ const FeedbackModalStyled = styled.div.attrs({
   width: 100%;
   height: 100%;
   background-color: ${(props) => props.theme.colors.blackAlpha};
+  backdrop-filter: blur(10px);
   color: ${(props) => props.theme.colors.white};
   font-family: ${(props) => props.theme.fonts.mainFont};
   font-size: 48px;
@@ -27,18 +28,27 @@ const FeedbackModalStyled = styled.div.attrs({
       justify-content: space-between;
 
       height: 215px;
-      width: 350px;
+      width: 320px;
     }
 
     &__head {
       display: flex;
-      align-items: center;
+      align-items: flex-start;
+      gap: 10px;
       justify-content: space-between;
+      font-size: ${(props) => props.theme.fontSizes.bigMessage};
+      padding-bottom: 10px;
     }
 
     &__title {
-      color: ${(props) => props.theme.colors.magenta};
-      font-weight: bold;
+      &--wrongfalse {
+        color: ${(props) => props.theme.colors.magenta};
+        font-weight: bold;
+      }
+      &--falsesuccess {
+        color: ${(props) => props.theme.colors.cyan};
+        font-weight: bold;
+      }
     }
   }
 `;
